2dc732101e697278ea3a2f305caf92dd9c015eac
Mario DeLaPaz <mariocd10@gmail.com>
2018-11-04 21:30:47 -0500

    S2L27: Create observable and subscribe to posts
    - removed code related to event binding
    - remove code related to property binding
    - The update improves effeciency

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,19 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   // public is typescript keyword that will create an object and save it with the value of type PostsService
-  constructor(public postsService: PostsService) {
+  constructor(public postsService: PostsService) {}
 
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
 
