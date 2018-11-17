731568b89135d8b421d028216f3270268fd8316a
Mario DeLaPaz <mariocd10@gmail.com>
2018-11-17 15:24:57 -0500

    S3L39: Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,20 @@ const express = require('express');
 // Express returns an app which is mostly middleware
 const app = express();
 
+// We want to execute this middlware before every request
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin' , '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+  next();
+});
+
 // First arg is the path to filter
 app.use('/api/posts', (req, res, next) => {
   const posts = [{
@@ -17,7 +31,7 @@ app.use('/api/posts', (req, res, next) => {
 
   res.status(200).json({
     message: 'Request successful!',
-    post: posts
+    posts: posts
   });
 });
 
