5a5d1c3b9cef2bd5efaba94ffd1e2161d4c62f4b
Mario DeLaPaz <mariocd10@gmail.com>
2018-11-23 20:43:24 -0500

    S4L54: Fetch data from db
    - use Post.find() to retrieve all documents

--- backend/app.js
+++ backend/app.js
@@ -47,20 +47,13 @@ app.post("/api/posts", (req, res, next) => {
 
 // First arg is the path to filter
 app.get('/api/posts', (req, res, next) => {
-  const posts = [{
-    id: 'awef434g3wq',
-    title: 'first post from server',
-    content: 'content from the server'
-  }, {
-    id: 'awasdfef434g3wq',
-    title: 'second post from server',
-    content: 'content'
-  }];
-
-  res.status(200).json({
-    message: 'Request successful!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Request successful!',
+        posts: documents
+      });
+    });
 });
 
 // export the express app to be used by the server
