0a9dfcf42b05eaf22398b85008d959e8627b653f
Mario DeLaPaz <mariocd10@gmail.com>
2019-02-26 21:07:59 -0500

    S8L97: Create User Model

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const postsRoutes = require("./routes/posts");
 const path = require('path');
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config')[env];
@@ -37,4 +39,5 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 module.exports = app;
